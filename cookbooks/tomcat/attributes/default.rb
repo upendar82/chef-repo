2c77053722a5493dbbc57a86c59c24a0812c2dcd
upendar82 <cheedalla.upendar@gmail.com>
2016-03-06 16:04:09 +0900

    Development Version Updated 14

--- cookbooks/tomcat/attributes/default.rb
+++ cookbooks/tomcat/attributes/default.rb
@@ -1,3 +1,3 @@
 node.default['tomcat']['s3_webapp_path'] = "s3://devopstraining"
-node.default['tomcat']['s3_webapp_development'] = "13210"
+node.default['tomcat']['s3_webapp_development'] = "143210"
 node.default['tomcat']['s3_webapp_production'] = "1"
--- cookbooks/tomcat/recipes/default.rb
+++ cookbooks/tomcat/recipes/default.rb
@@ -7,9 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 if node.environment == "development"
-artifact_dir = node['web_app']['s3_webapp_development']
+artifact_dir = node['tomcat']['s3_webapp_development']
 elsif node.environment == "production"
-artifact_dir = node['web_app']['s3_webapp_production']
+artifact_dir = node['tomcat']['s3_webapp_production']
+
+
+puts "Artifact Dir : #{artifact_dir}"
 
 execute "remove_old_jar"
   user "root"
@@ -21,5 +24,5 @@ execute "get_tomcat_war" do
   user "root"
   group "root"
   cwd  "/usr/local/tomcat7/webapps"
-  command "s3cmd get -f #{node['web_app']['s3_webapp_path']}/#{artifact_dir}/com.webdemo.war"
+  command "s3cmd get -f #{node['tomcat']['s3_webapp_path']}/#{artifact_dir}/com.webdemo.war"
  end
