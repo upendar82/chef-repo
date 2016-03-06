f98e7ece0a7eb67df8b32e04c51dfc52e298fe04
upendar82 <cheedalla.upendar@gmail.com>
2016-03-06 14:19:46 +0900

    Development Version Updated 9

--- cookbooks/tomcat/attributes/default.rb
+++ cookbooks/tomcat/attributes/default.rb
@@ -1,3 +1,3 @@
 node.default['web_app']['s3_webapp_path'] = "s3://devopstraining"
-node.default['web_app']['s3_webapp_development'] = "1"
+node.default['web_app']['s3_webapp_development'] = "TOMCAT_BUILD_NUMBER"
 node.default['web_app']['s3_webapp_production'] = "1"
